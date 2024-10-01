eb4131c6a75f682264a38671d4f37633f6101a3e
abdul ansari <samunleash1@yahoo.com>
2024-10-01 19:02:04 +0400

    header reset resolved

--- src/pages/api/backend/[...slug].js
+++ src/pages/api/backend/[...slug].js
@@ -7,10 +7,14 @@ export default async function handler(req, res) {
     for (let d of slug) {
       path = path + `/${d}`
     }
-    const { data } = await axios.post(`${process.env.baseUrl}${path}`, req.body, { headers: req.headers })
+    console.log(path)
+    console.log(req.headers)
+    const { data } = await axios.post(`${process.env.baseUrl}${path}`, req.body, {
+      headers: { token: req.headers.token }
+    })
     res.status(200).json(data)
   } catch (error) {
-    console.error(error.response.data)
-    res.status(error.response.status || 500).end(error.response.data)
+    console.error(error)
+    res.status(500).end(error)
   }
 }
