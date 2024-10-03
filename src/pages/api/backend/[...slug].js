32afb55b42dc27c78ecbbd518a5afdb931f39731
Sufiyan <sufiyanmemon687@gmail.com>
2024-10-03 16:35:34 +0530

    stash code sync

--- src/pages/api/backend/[...slug].js
+++ src/pages/api/backend/[...slug].js
@@ -13,6 +13,6 @@ export default async function handler(req, res) {
     res.status(200).json(data)
   } catch (error) {
     console.error(error)
-    res.status(error.response || 500).end(error.response)
+    res.status(500).end(error)
   }
 }
